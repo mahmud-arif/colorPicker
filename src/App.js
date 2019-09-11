946f496cc56a13977303516846f41c9d301e04f5
mahmud <mahmudarif1175@gmail.com>
2019-09-11 20:21:08 +0600

    fix palette route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import {Router, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router'
 import colorSeeds from './seedColors'; 
 import { generatePalette } from './colorHelper'; 
 import Palette from './Palette'; 
 
 class App extends Component {
+  findPalette = id => { return colorSeeds.find(color => color.id === id) }; 
+
+  
   render() {
-    console.log(generatePalette(colorSeeds[4])); 
-    return(
-      <div >
-        <Palette palette={generatePalette(colorSeeds[4])}/>
-      </div>
+    console.log(this.findPalette('flat-ui-colors-aussie'))
+    return (
+      
+      <Switch>
+        <Route exact path="/" render={() => <div>root page</div>} />
+        <Route
+          exact path="/palette/:id"
+          render={routerProps => <Palette palette={generatePalette(this.findPalette(routerProps.match.params.id))} />} />
+      </Switch>
+      // <div >
+      //   <Palette palette={generatePalette(colorSeeds[4])}/>
+      // </div>
     );
   }
 }
